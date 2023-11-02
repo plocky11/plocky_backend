69aadb52abef34cc0a50ffa7dff35e4dbd942527
sungjun4403 <geulligu89@naver.com>
2023-11-02 09:22:06 +0900

    🎉: create new spring project

---
+++ plocky/src/test/java/com/plocky/PlockyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plocky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlockyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
