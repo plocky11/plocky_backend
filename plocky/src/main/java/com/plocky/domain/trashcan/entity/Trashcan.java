9af8eeb50ce0e29599ff84ff8250165ac4eca4da
ppparkta <pppppark2@gmail.com>
2023-11-05 00:29:50 +0900

    feat: plogging, trashcategory, trashcan, basetime 엔티티 개발 #3

--- plocky/src/main/java/com/plocky/domain/trashcan/entity/Trashcan.java
+++ plocky/src/main/java/com/plocky/domain/trashcan/entity/Trashcan.java
@@ -1,4 +1,20 @@
 package com.plocky.domain.trashcan.entity;
 
-public class Trashcan {
+import com.plocky.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Transactional
+public class Trashcan extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="trashcan_id")
+    private Long id;
+    private String coordinate;
 }
