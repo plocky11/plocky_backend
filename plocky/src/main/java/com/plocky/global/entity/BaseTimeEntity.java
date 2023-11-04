31522afd8b4766c879f6b19fc3502a3b12a08562
ppparkta <pppppark2@gmail.com>
2023-11-05 05:03:09 +0900

    feat: kakao 연동(로그인URI, 인가코드 및 엑세스토큰 발급, 회원정보 조회)#1

--- plocky/src/main/java/com/plocky/global/entity/BaseTimeEntity.java
+++ plocky/src/main/java/com/plocky/global/entity/BaseTimeEntity.java
@@ -3,8 +3,8 @@ package com.plocky.global.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Builder;
-import lombok.Getter;
+import jakarta.transaction.Transactional;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,9 +12,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@EntityListeners(value={AuditingEntityListener.class})
 @MappedSuperclass
 @Getter
-@EntityListeners(value={AuditingEntityListener.class})
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+@Transactional
 public class BaseTimeEntity {
     @Column(updatable = false)
     @CreatedDate
