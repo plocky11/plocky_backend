5770035b1f939b15a74f385bfc8419a3cba644cd
sungjun4403 <geulligu89@naver.com>
2023-11-05 00:22:42 +0900

    🔨 feat: memeber entity dev (unrelated)

---
+++ plocky/src/main/java/com/plocky/domain/member/entity/Member.java
@@ -0,0 +1,37 @@
+package com.plocky.domain.member.entity;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+@Transactional
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+    private String refreshToken;
+    private String nickname;
+    private float totalDistance;
+    private Integer totalQuantity;
+
+//    @
+//    List<Plogging> ploggingList = new ArrayList<>();
+
+//    @OneToOne
+//    private Pet pet;
+
+}
